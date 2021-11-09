8e7bcb6102e6fa28169cc7a17fb1795583e77ab7
Abhishek Sharma <abhishekalien09@gmail.com>
2021-11-09 20:35:22 +0530

    Merged codeforces api and draging feature

--- script.js
+++ script.js
@@ -18,35 +18,15 @@ window.onload = function () {
         updateDrag();
     }
 };
+
+
 document.getElementById("add").addEventListener('click', e => {
     e.preventDefault();
     const div = document.querySelector(".task-div");
     let text = document.querySelector(".inputTask").value;
     text = text.trim();
     if (text != '') {
-        const check = document.createElement('input');
-        check.setAttribute("type", "checkbox");
-        check.className = "checkbox";
-        check.setAttribute("value", "");
-        const output = document.createElement('li')
-        output.innerText = text;
-
-        const b1 = document.createElement('button');
-        b1.className = 'up';
-        b1.innerHTML = `<i class="fas fa-angle-up"></i>`;
-        b1.setAttribute("title", "MoveUp")
-        const b2 = document.createElement('button');
-        b2.className = 'down';
-        b2.innerHTML = `<i class="fas fa-angle-down"></i>`;
-        b2.setAttribute("title", "MoveDown")
-        const task = document.createElement('div');
-        task.className = "task";
-        task.setAttribute("draggable",true);
-        task.appendChild(check);
-        task.appendChild(b1);
-        task.appendChild(b2);
-        task.appendChild(output);
-        task.querySelector('.checkbox').addEventListener('change', check)
+        let task = createTodo(text);
         if (tasks.length == 0)
             taskdiv.querySelector(".no").remove();
         div.appendChild(task);
@@ -55,7 +35,7 @@ document.getElementById("add").addEventListener('click', e => {
         update();
         updateDrag();
     }
-    
+
 })
 document.getElementById("sort").addEventListener('click', e => {
     e.preventDefault();
@@ -94,6 +74,33 @@ document.getElementById("delete").addEventListener('click', e => {
     }
     update();
 })
+
+function createTodo(text) {
+    const check = document.createElement('input');
+    check.setAttribute("type", "checkbox");
+    check.className = "checkbox";
+    check.setAttribute("value", "");
+    const output = document.createElement('li')
+    output.innerText = text;
+
+    const b1 = document.createElement('button');
+    b1.className = 'up';
+    b1.innerHTML = `<i class="fas fa-angle-up"></i>`;
+    b1.setAttribute("title", "MoveUp")
+    const b2 = document.createElement('button');
+    b2.className = 'down';
+    b2.innerHTML = `<i class="fas fa-angle-down"></i>`;
+    b2.setAttribute("title", "MoveDown")
+    const task = document.createElement('div');
+    task.className = "task";
+    task.setAttribute("draggable", true);
+    task.appendChild(check);
+    task.appendChild(b1);
+    task.appendChild(b2);
+    task.appendChild(output);
+    task.querySelector('.checkbox').addEventListener('change', check)
+    return task;
+}
 function goUp(e) {
     let parentElement = e.target.parentElement;
     if (parentElement.className == "up") {
@@ -139,10 +146,12 @@ function check() {
     if (ele.querySelector('.checkbox').checked) {
         ele.querySelector('.checkbox').value = "checked";
         ele.getElementsByTagName('li')[0].innerHTML = `<strike>${text}</strike>`;
+        ele.style.backgroundColor = "rgb(53 175 54 / 50%)";
     }
     else {
         ele.querySelector('.checkbox').value = '';
         ele.getElementsByTagName('li')[0].innerHTML = `${text}`;
+        ele.style.backgroundColor = "rgb(255 255 255 / 0%)";
     }
     updateStorage();
 }
@@ -156,9 +165,11 @@ function updateCheckBox() {
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].querySelector(".checkbox").value == "checked") {
             tasks[i].querySelector(".checkbox").checked = true;
+            tasks[i].style.backgroundColor = "rgb(53 175 54 / 50%)";
         }
         else {
             tasks[i].querySelector(".checkbox").checked = false;
+            tasks[i].style.backgroundColor = "rgb(255 255 255 / 0%)";
         }
     }
 
@@ -188,33 +199,43 @@ function updateStorage() {
     }
     localStorage.setItem("todos", JSON.stringify(todos));
 }
-function updateDrag(){
+function updateDrag() {
     let taskArray = document.querySelectorAll(".task");
     let dropTask = null;
     taskArray.forEach(task => {
-        task.setAttribute("draggable",true);
-        task.addEventListener("dragstart",e=>{
+        task.setAttribute("draggable", true);
+        task.addEventListener("dragstart", e => {
             // console.log("start");
             e.target.classList.add('dragging');
+            start = e.target;
+            if (start.className == "")
+                start = start.parentElement;
+            // console.log("start" , e.target)
         })
-        task.addEventListener("dragend",e=>{
+        task.addEventListener("dragend", e => {
             e.preventDefault();
-            let currEle = e.target ; 
-            let temp = currEle.innerHTML ;
-            currEle.innerHTML = dropTask.innerHTML ; 
-            dropTask.innerHTML = temp ; 
-            e.target.classList.remove('dragging');
-            // console.log("end")
+            let currEle = document.querySelector('.dragging');
+            // console.log("end",currEle , dropTask);
+            if (currEle != null) {
+                let temp = currEle.innerHTML;
+                currEle.innerHTML = dropTask.innerHTML;
+                dropTask.innerHTML = temp;
+                e.target.classList.remove('dragging');
+            }
             update();
         })
-        task.addEventListener("dragover" ,e=>{
-            e.preventDefault(); 
-            // console.log("over");
+        task.addEventListener("dragover", e => {
+            e.preventDefault();
+
+            // console.log("over" , e.target);
             taskArray.forEach(taskele => {
-                let top = taskele.offsetTop ; 
-                let bottom = top + taskele.offsetHeight + 6 ; 
-                if(e.clientY >= top && e.clientY <= bottom){
-                    dropTask = taskele ;
+                let top = taskele.offsetTop;
+                let bottom = top + taskele.offsetHeight + 6;
+                if (e.clientY >= top && e.clientY <= bottom) {
+                    dropTask = taskele;
+                    if (dropTask.className == '')
+                        dropTask = dropTask.parentElement;
+                    // console.log("over" , taskele);
                 }
             })
         })
@@ -234,5 +255,77 @@ function update() {
     }
     updateStorage();
 }
+
+document.getElementById("cf").addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(".filter-section").style.display = "block";
+    document.getElementById("tags").addEventListener("change", e => {
+        e.preventDefault();
+        const listdiv = document.createElement('div')
+        listdiv.className = "taglist";
+        const li = document.createElement('li');
+        li.className = "tagId"
+        li.innerHTML = `${document.getElementById("tags").value}`;
+        const btn = document.createElement('button')
+        btn.innerHTML = "X";
+        btn.className = "cross";
+        listdiv.append(li);
+        listdiv.append(btn);
+        document.querySelector(".selectedtags").appendChild(listdiv);
+        let crosses = document.getElementsByClassName("cross");
+        for (let i = 0; i < crosses.length; i++) {
+            crosses[i].addEventListener('click', e => {
+                const par = e.target.parentElement;
+                par.remove();
+            })
+        }
+        document.querySelector(".selectedtags").scrollTop = document.querySelector(".selectedtags").scrollHeight;
+    })
+    document.getElementById("API").addEventListener('click', e => {
+        e.preventDefault();
+        e.preventDefault();
+        let tags = "";
+        let taglist = document.getElementsByClassName("tagId");
+        for (let i = 0; i < taglist.length; i++) {
+            tags += `${taglist[i].innerText};`;
+        }
+        let start = document.querySelector(".startR").value  ; 
+        let end = document.querySelector(".stopR").value ; 
+        console.log(tags);
+        str = `https://codeforces.com/api/problemset.problems?tags=${tags}`
+
+        fetch(str,
+            {
+                method: "GET"
+            }
+        ).then(res => res.json())
+            .then(data => {
+                let problems = [];
+                if(start > 0 && end > 0){
+                for(let i = 0 ; i < data.result.problems.length ; i++){
+                    let currProblem = data.result.problems[i] ;
+                    if(currProblem.rating >= start && currProblem.rating <= end)
+                        problems.push(currProblem);
+                }}
+                else
+                    problems = data.result.problems;
+                // console.log(problems);
+                for (let i = 0; i < 4; i++) {
+                    var idx = Math.floor(Math.random() * problems.length);
+                    let task = createTodo('');
+                    const li = task.getElementsByTagName('li')[0];
+                    li.innerHTML = `<a target="_blank" href = "https://codeforces.com/problemset/problem/${problems[idx].contestId}/${problems[idx].index}">${problems[idx].name}</a>`;
+                    taskdiv.appendChild(task);
+                }
+
+                document.querySelector(".filter-section").style.display = "none";
+                update(); 
+                updateDrag(); 
+            })
+    })
+
+})
+
+
 
 
